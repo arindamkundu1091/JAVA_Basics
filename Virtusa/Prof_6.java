44da4c2f0d5102dd761faea1fbf57467cd03ae25
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-15 16:27:29 +0530

    ...

--- Virtusa/Prof_6.java
+++ Virtusa/Prof_6.java
@@ -67,7 +67,7 @@ class NonTeachingStaff extends Staff {
     }
 }
 
-class Prof {
+class Prof_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
