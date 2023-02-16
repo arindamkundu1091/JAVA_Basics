3f662f72b8871eda7dcb0cb71730f3ac67c09eaa
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-16 09:41:43 +0530

    ...

---
+++ Virtusa/Card_8.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class VISACard {
+    String holderName;
+    String ccv;
+
+    public double computeRewardPoints(String purchaseType, double amount) {
+        double reward = amount * 0.01;
+        return reward;
+    }
+}
+
+class HPVISACard extends VISACard {
+    public double computeRewardPoints(String purchaseType, double amount) {
+        double reward = super.computeRewardPoints(purchaseType, amount);
+        if (purchaseType.equals("fuel")) {
+            reward = reward + 10;
+            return reward;
+        } else {
+            return reward;
+        }
+    }
+}
+
+public class Card_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        String holderName = sc.nextLine();
+        String ccv = sc.nextLine();
+        double amount = sc.nextDouble();
+        sc.nextLine();
+        String type = sc.nextLine();
+        switch (t) {
+            case 1: {
+                VISACard vc = new VISACard();
+                double reward = vc.computeRewardPoints(type, amount);
+                System.out.println(reward);
+            }
+                break;
+            case 2: {
+                HPVISACard hvc = new HPVISACard();
+                double reward = hvc.computeRewardPoints(type, amount);
+                System.out.println(reward);
+            }
+                break;
+        }
+        sc.close();
+    }
+}
---
+++ Virtusa/Event_4.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class Event {
+    String name;
+    String detail;
+    String type;
+    String ownerName;
+    Event(){
+        
+    }
+    Event(String name, String detail, String type, String ownerName) {
+        this.name = name;
+        this.detail = detail;
+        this.type = type;
+        this.ownerName = ownerName;
+    }
+}
+
+class Exhibition extends Event {
+    int noOfStalls;
+    Exhibition() {
+        super();
+    }
+    Exhibition(String name, String detail, String type, String ownerName, int noOfStalls) {
+        super(name, detail, type, ownerName);
+        this.noOfStalls = noOfStalls;
+    }
+    void showExData() {
+        System.out.println(name+" "+detail+" "+type+" "+ownerName+" NoOfStalls: "+noOfStalls);
+    }
+}
+
+class StageEvent extends Event {
+    int noOfSeats;
+    StageEvent() {
+        super();
+    }
+    StageEvent(String name, String detail, String type, String ownerName, int noOfSeats) {
+        super(name, detail, type, ownerName);
+        this.noOfSeats = noOfSeats;
+    }
+    void showSeData() {
+        System.out.println(name+" "+detail+" "+type+" "+ownerName+" NoOfSeats: "+noOfSeats);
+    }
+}
+
+public class Event_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        String data[] = sc.nextLine().trim().split(",");
+        String name =  data[0];
+        String detail = data[1];
+        String type = data[2];
+        String ownerName = data[3];
+        int nos = Integer.parseInt(data[4]);
+        switch (t) {
+            case 1: {
+                Exhibition ex = new Exhibition(name, detail, type, ownerName, nos);
+                ex.showExData();
+                // System.out.println(name+" "+detail+" "+type+" "+ownerName+" NoOfSeats: "+nos);
+            }
+                break;
+            case 2: {
+                StageEvent se = new StageEvent(name, detail, type, ownerName, nos);
+                se.showSeData();
+                // System.out.println(name + " " + detail + " " + type + " " + ownerName + " NoOfSeats: " + nos);
+            }
+                break;
+            default: {
+                System.out.println("Invalid Choice");
+            }
+                break;
+        }
+        sc.close();
+    }
+}
