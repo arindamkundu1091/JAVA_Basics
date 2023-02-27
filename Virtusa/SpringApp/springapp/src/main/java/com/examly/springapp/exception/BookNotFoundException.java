ae61ced7053b4c2313b63887a9dff1042a9fabe0
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-27 19:14:24 +0530

    ...

--- Virtusa/SpringApp/springapp/src/main/java/com/examly/springapp/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.examly.springapp.exception;
-
-public class BookNotFoundException extends Exception {
-    public BookNotFoundException(long bookId) {
-        super("Could not find book with id " + bookId);
-    }
-}
